d77bb5440f2acca42ad172e22650f4a59488defc
APOM-Tawan <pathompong.cham@mail.kmutt.ac.th>
2023-03-28 15:23:22 +0700

    Lab3 Final

---
+++ Lab#3/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+//import Home from "./components/Home";
+//import Todo from "./components/Todo";
+import TodoPage from "./Pages/TodoPage";
+function App(){
+    return (
+        <TodoPage/>
+    );
+}
+export default App;
